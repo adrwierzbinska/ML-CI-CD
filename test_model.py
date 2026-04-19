7e813862e8100d8f9377e498175ecc85a7cbc285
adrwierzbinska <adriannawierzbinska@gmail.com>
2026-04-19 18:25:19 +0200

    test update

--- test_model.py
+++ test_model.py
@@ -1,32 +1,28 @@
 import numpy as np
-from model import train_and_predict, get_accuracy
+from app import y_pred, y_test, model_accuracy
 
 def test_predictions_not_none():
     """
     Test 1: Sprawdza, czy otrzymujemy jakąkolwiek predykcję.
     """
-    preds, _ = train_and_predict()
-    assert preds is not None, "Predykcje nie powinny być None."
+    assert y_pred is not None, "Predykcje nie powinny być None."
 
 def test_predictions_length():
     """
-    Test 2 (na maksymalną ocenę 5): Sprawdza, czy długość listy predykcji jest większa od 0 i czy odpowiada przewidywanej liczbie próbek testowych.
+    Test 2: Sprawdza, czy długość listy predykcji jest większa od 0 i czy odpowiada przewidywanej liczbie próbek testowych.
     """
-    preds, y_test = train_and_predict()
-    assert len(preds) > 0, "Predykcje nie mogą być puste."
-    assert len(preds) == len(y_test), "Liczba predykcji musi odpowiadać liczbie próbek testowych."
+    assert len(y_pred) > 0, "Predykcje nie mogą być puste."
+    assert len(y_pred) == len(y_test), "Liczba predykcji musi odpowiadać liczbie próbek testowych."
 
 def test_predictions_value_range():
     """
-    Test 3 (na maksymalną ocenę 5): Sprawdza, czy wartości w predykcjach mieszczą się w spodziewanym zakresie: Dla zbioru Iris mamy 3 klasy (0, 1, 2).
+    Test 3: Sprawdza, czy wartości w predykcjach mieszczą się w spodziewanym zakresie.
     """
-    preds, _ = train_and_predict()
-    for p in preds:
-        assert p in [0, 1, 2], f"Predykcja {p} jest poza dozwolonym zakresem klas (0, 1, 2)."
+    for p in y_pred:
+        assert p in [0, 1], f"Predykcja {p} jest poza dozwolonym zakresem klas (0, 1)."
 
 def test_model_accuracy():
     """
-    Test 4 (na maksymalną ocenę 5): Sprawdza, czy model osiąga co najmniej 70% dokładności (przykładowy warunek, można dostosować do potrzeb).
+    Test 4: Sprawdza, czy model osiąga co najmniej 70% dokładności.
     """
-    accuracy = get_accuracy()
-    assert accuracy >= 0.70, f"Dokładność modelu ({accuracy * 100}%) jest mniejsza niż wymagane 70%."
+    assert model_accuracy >= 0.70, f"Dokładność modelu ({model_accuracy * 100}%) jest mniejsza niż wymagane 70%."
